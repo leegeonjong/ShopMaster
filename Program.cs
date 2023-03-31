88bcaff39d99c1c595222fcacd6501ff2f4addec
leegeonjong <koka85260@gmail.com>
2023-03-31 10:25:33 +0900

    initial commit

---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ShopMaster
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("header");
+            {
+
+            }
+            Console.WriteLine("article");
+            {
+
+            }
+            Console.WriteLine("footer");
+            {
+
+            }
+        }
+
+    }
+}
